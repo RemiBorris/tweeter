b4472f69f86390ccc7adeb35ed3b11acef8f8972
remiborris <remiborris@gmail.com>
2024-07-08 11:26:05 -0300

    Add jQuery to change the counter on input into textarea for remaining characters to not exceed 140

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  // Set up the event listener for the textarea
+  $('.new-tweet textarea').on('input', function(event) {
+    let textLength = $(this).val().length;
+    let remainingChars = 140 - textLength;
+
+    // Use .closest and .find to target the counter element
+    $(this).closest('form').find('.counter').text(remainingChars);
+
+    // Change the color if character limit is exceeded
+    if (remainingChars < 0) {
+      $(this).closest('form').find('.counter').css('color', 'red');
+    } else {
+      $(this).closest('form').find('.counter').css('color', 'inherit');
+    }
+  });
+});
