77f8f20d4fa661830db9698a4041cf9fae343e11
remiborris <remiborris@gmail.com>
2024-07-10 16:16:17 -0300

    create ajax request to pull database request and render the tweets onto the page and implement timeago for proper time formatting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
           <p>${tweetObject.content.text}</p>
           <hr>
           <footer>
-            <h5>${tweetObject.created_at}</h5>
+            <h5>${timeago.format(tweetObject.created_at)}</h5>
             <div id="icons">
               <i class="fa-solid fa-flag"></i>
               <i class="fa-solid fa-retweet"></i>
@@ -35,35 +35,30 @@ $(document).ready(function() {
   const renderTweets = function(arrayOfTweets) {
     for (let tweetObject of arrayOfTweets) {
       const $tweet = createTweetElement(tweetObject);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  $("#new-tweet-form").on("submit", function(event) {
+    event.preventDefault();
+    const formData = $("#new-tweet-form").serialize();
+
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: formData,
+    });
+  });
 
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+      success: data => renderTweets(data)
+    });
+  };
+
+  loadTweets();
+
 });
