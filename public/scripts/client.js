95f7caf410a78b4198f923fee98f47c280e0f674
remiborris <remiborris@gmail.com>
2024-07-21 14:24:08 -0300

    fix submit action to reset character count and reset form before submitting request, also add comments so our code is more legible to follow

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,6 +7,9 @@
 /*global $ document timeago*/
 $(document).ready(function() {
 
+  // Below code creates the tweet element in steps
+
+  // We start by creating the footer element of the tweet element which includes timestamps and icons
   const createTweetFooter = function(timestamp) {
     let footer = $(`<footer>`);
     let h5 = $(`<h5>`);
@@ -25,6 +28,7 @@ $(document).ready(function() {
     return footer;
   };
 
+  // We then create the header of the tweet which includes the username, avatar and handle
   const createTweetHeader = function(user) {
     let header = $(`<header>`);
     let avatar = $(`<img>`);
@@ -42,16 +46,19 @@ $(document).ready(function() {
     return header;
   };
 
+  // Lastly the each element is  added together in order to form the entire tweet with the header, content and footer
   const createTweetElement = function(tweetObject) {
     let article = $(`<article class="tweet-article">`);
     let content = $(`<p>`);
     content.text(tweetObject.content.text);
     article.append(createTweetHeader(tweetObject.user));
     article.append(content);
+    article.append($(`<hr>`));
     article.append(createTweetFooter(tweetObject.created_at));
     return article;
   };
 
+  // Function to loop through the database to add each tweet to the web page
   const renderTweets = function(arrayOfTweets) {
     for (let tweetObject of arrayOfTweets) {
       const tweet = createTweetElement(tweetObject);
@@ -59,8 +66,10 @@ $(document).ready(function() {
     }
   };
 
+  // We ensure that no error message is on the page to begin
   $('#error-message').hide();
 
+  // Function used to create a GET to the database to return the tweets
   const loadTweets = function() {
     $.ajax({
       method: 'GET',
@@ -72,10 +81,12 @@ $(document).ready(function() {
     });
   };
 
+  // Below is triggered when we "submit" the tweet by pressing the tweet button
   $("#new-tweet-form").on("submit", function(event) {
     event.preventDefault();
     const formData = $("#new-tweet-form").serialize();
     
+    // Verify if the conditions are met for character counts
     if ($('#tweet-text').val() === "") {
       $('#error-message').text('⚠ Too short. Plz rspct our mandatory minimum of 1 char. #kthxbye. ⚠');
       $('#error-message').show(400);
@@ -88,6 +99,10 @@ $(document).ready(function() {
       return;
     }
 
+    // Reset the form so we do not accidentaly submit the form twice
+    $('#new-tweet-form')[0].reset();
+
+    // Create a POST request to the server to submit our tweet, once successful hide any error messages and reload the tweets
     $.ajax({
       method: 'POST',
       url: '/tweets',
@@ -96,6 +111,7 @@ $(document).ready(function() {
         loadTweets();
         $('#error-message').hide(300);
         $('#error-message').empty();
+        $('.counter').text(140);
       }
     });
   
